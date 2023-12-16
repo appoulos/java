384fbca3ecf34ebb350acd2ac1a580fc2bf7b4ca
Anthony Poulos <anthonypoulos@yahoo.com>
2023-12-15 21:36:34 -0600

    .

--- ScoreBoard.java
+++ ScoreBoard.java
@@ -5,21 +5,20 @@ import java.util.regex.Pattern;
 class ScoreBoard {
   static String aName = "a:";
   static String bName = "b:";
-  static int width = 10;
-
-  static String scoreAddFormat = "+" + repeat(" ",aName.length()) + 
-    "%" + width + "d   " +
-    repeat(" ",bName.length()) +
-    "%" + width + "d\n";
-
-  static String scoreTotFormat = "%s %" + width + "d  %s %" + width + "d";
+  static int minWidth = 10;
+  static String prompt; // = "> "; // set in main()
+  static String scoreTotFormat = "%s %" + minWidth + "d  %s %" + minWidth + "d";
 
   public static void helpVersion2() {
-    System.out.println("Enter one or two numbers separated by whitespace and press return.\nTo add only to b's score enter a space and only one number");
+    System.out.println("# Enter one or two numbers separated by whitespace and press return.");
+    System.out.println("  To add only to b's score enter a space and only one number");
+    prompt = "# ";
   }
 
   public static void helpVersion1() {
-    System.out.println("Enter a number and press return to add to a's score.\nTo add to b's score enter whitespace (tab or a space) and a number");
+    System.out.println("$ Enter a number and press return to add to a's score.");
+    System.out.println("  To add to b's score enter whitespace (tab or a space) and a number");
+    prompt = "$ ";
   }
 
   public static void help() {
@@ -30,41 +29,28 @@ class ScoreBoard {
     System.out.println("  \"s\" to switch input style (one or two numbers)");
   }
 
-  public static String repeat(String s, int times) {
-    String r = "";
-    for (int i=0; i < times; i++) {
-      r += s;
-    }
-    return r;
-  }
-
   public static void score(BigInteger a, BigInteger b) {
-    int aWidth = Math.max(width, a.toString().length());
-    int bWidth = Math.max(width, b.toString().length());
-    System.out.println( repeat("=", 4 + 
+    int aWidth = Math.max(minWidth, a.toString().length());
+    int bWidth = Math.max(minWidth, b.toString().length());
+    System.out.println( "=".repeat(4 + 
           aName.length() + 
           bName.length() + 
           aWidth +
           bWidth ));
     System.out.printf(scoreTotFormat, aName, a, bName, b);
-    System.out.print("  $ ");
+    System.out.print("  " + prompt);
   }
 
   public static void scoreAdd(BigInteger a, BigInteger addA, BigInteger b, BigInteger addB) {
-    int aWidth = width;
-    aWidth = Math.max(aWidth, a.toString().length());
-    aWidth = Math.max(aWidth, addA.toString().length());
-    // System.out.println("aWidth: " + aWidth);
-    int bWidth = width;
-    bWidth = Math.max(bWidth, b.toString().length());
-    bWidth = Math.max(bWidth, addB.toString().length());
+    int aWidth = Math.max(minWidth, a.toString().length());
+    int bWidth = Math.max(minWidth, b.toString().length());
 
-    String scoreAddFormat = "+" + repeat(" ",aName.length()) + 
-    "%" + aWidth + "d   " +
-    repeat(" ",bName.length()) +
-    "%" + bWidth + "d\n";
+    String scoreAddFormat = "+" + " ".repeat(aName.length()) + 
+      "%" + aWidth + "d   " +
+      " ".repeat(bName.length()) +
+      "%" + bWidth + "d\n";
+
     System.out.printf(scoreAddFormat, addA, addB);
-
   }
 
   public static void main(String[] args) {
@@ -83,10 +69,10 @@ class ScoreBoard {
     String s = ""; // input string
 
     // following 2 lines are for version1
-    BigInteger i = new BigInteger("0"); // input converted to int
+    BigInteger input = new BigInteger("0"); // input converted to int
     boolean addToB = false;
 
-    Pattern pattern = Pattern.compile("([-0-9]*)(\\s*)([-0-9]*)"); // regex (version2)
+    Pattern pattern = Pattern.compile("(\\s*)([-0-9]*)(\\s*)([-0-9]*)"); // regex (version2)
 
     score(a, b);
     while (true) {
@@ -119,62 +105,59 @@ class ScoreBoard {
           }
         }
 
-        if (useVersion2) { // regex
+        if (useVersion2) { // version2 (regex)
           Matcher matcher = pattern.matcher(s);
-          // while (matcher.find()) { // this gets a little crazy for trying to enter many numbers per line i.e. "1 2 3 4"
+          // while (matcher.find()) { // this gets a little crazy for trying to enter many numbers per line ie. "1 2 3 4"
           if (matcher.find()) {
-            // System.out.println("group 1: " + matcher.group(1) + ".");
-            // System.out.println("group 2: " + matcher.group(2) + ".");
-            // System.out.println("group 3: " + matcher.group(3) + ".");
-            if (matcher.group(1).length() > 0) {
-              addA = new BigInteger(matcher.group(1));
-              // addA = Integer.parseInt(matcher.group(1));
-            } else {
+            // debug the regex groups:
+            // for (int i = 1; i <= 4; ++i) {
+            //   System.out.println("group " + i + ": " + matcher.group(i) + ".");
+            // }
+            try {
+              if (matcher.group(1).length() > 0) { // starts with whitespace so only add to b
+                addA = new BigInteger("0");
+                addB = new BigInteger(matcher.group(2));
+              } else {
+                addA = new BigInteger(matcher.group(2));
+                addB = new BigInteger(matcher.group(4));
+              }
+            } catch (NumberFormatException e) {
+              System.out.print("Invalid number, ");
               addA = new BigInteger("0");
-            }
-            if (matcher.group(3).length() > 0) {
-              addB = new BigInteger(matcher.group(3));
-              // addB = Integer.parseInt(matcher.group(3));
-            } else {
               addB = new BigInteger("0");
             }
           }
-        } else { // non-regex
+        } else { // version1 (non-regex)
           if (s.length() > 0 && Character.isWhitespace(s.charAt(0))) { 
-            // == ' ' || s.charAt(0) == '\t'
-            s = s.trim(); // System.out.print(", s: " + s);
+            s = s.trim();
             addToB = true;
           } else {
             addToB = false;
           }
           try {
-            // i = Integer.parseInt(s);
-            i = new BigInteger(s);
+            input = new BigInteger(s);
           } catch (NumberFormatException e) {
-            System.out.println("Invalid number");
-            // i = 0;
-            i = new BigInteger("0");
+            System.out.print("Invalid number, ");
+            input = new BigInteger("0");
           } catch (Exception e) {
             e.printStackTrace();
           }
 
           if (addToB) {
             addA = new BigInteger("0");
-            addB = i;
+            addB = input;
           } else {
-            addA = i;
+            addA = input;
             addB = new BigInteger("0");
           }
         }
 
-        if (addA.compareTo(BigInteger.ZERO) > 0 && addB.compareTo(BigInteger.ZERO) > 0) {
+        if (addA.compareTo(BigInteger.ZERO) == 0 && addB.compareTo(BigInteger.ZERO) == 0) {
           System.out.println("No change!");
         } else {
-          // System.out.printf(scoreAddFormat, addA, addB);
           scoreAdd(a, addA, b, addB);
           a = a.add(addA);
           b = b.add(addB);
-          // b += addB;
         }
 
         score(a, b);
