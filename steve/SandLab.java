6d6ec537ef04e08c9646a6425aba2aed9124ae48
Anthony Poulos <anthonypoulos@yahoo.com>
2025-01-15 11:54:13 -0600

    .

--- steve/SandLab.java
+++ steve/SandLab.java
@@ -14,7 +14,11 @@ public class SandLab
     public static final int METAL = 1;
     public static final int SAND = 2;
     public static final int WATER = 3;
-    public static final int LASER = 4;
+    public static final int WOOD = 4;
+    public static final int FIRE = 5;
+    public static final int LASER = 6;
+    public static final int NUKE = 7;
+    
 
     //do not add any more fields
     private int[][] grid;
@@ -23,22 +27,36 @@ public class SandLab
     public SandLab(int numRows, int numCols)
     {
         String[] names;
-        names = new String[5];
-        names[ERASE] = "ERASE";
+        names = new String[8];
         names[METAL] = "Metal";
         names[SAND] = "Sand";
         names[WATER] = "Water";
+        names[WOOD] = "Wood";
+        names[FIRE] = "Fire";
         names[LASER] = "Laser";
+        names[NUKE] = "NUKE...";
+        names[ERASE] = "ERASE";
         grid = new int[numRows][numCols];
         display = new SandDisplay("Falling Sand", numRows, numCols, names);
     }
 
-//called when the user clicks on a location using the given tool
+    //called when the user clicks on a location using the given tool
     private void locationClicked(int row, int col, int tool)
     {
-        if (grid[row][col] == ERASE && tool != LASER && tool != ERASE) {
+       // if (grid[row][col] == ERASE || tool == LASER) {
             grid[row][col] = tool;
-        }
+            // make laser 3 wide
+            if (tool == LASER) {
+                // down and left
+                if (row + 1 < grid.length - 1 && col - 1 >= 0) {
+                    grid[row + 1][col - 1] = tool;
+                }
+                // up and right
+                if (row - 1 >= 0 && col + 1 <= grid[0].length - 1) {
+                    grid[row - 1][col + 1] = tool;
+                }
+            }
+       // }
     }
 
     //copies each element of grid into the display
@@ -50,36 +68,132 @@ public class SandLab
                     display.setColor(row, col, Color.gray);
                 } else if (grid[row][col] == SAND) {
                     display.setColor(row, col, Color.yellow);
+                } else if (grid[row][col] == WATER) {
+                    display.setColor(row, col, Color.blue);
                 } else if (grid[row][col] == ERASE) {
                     display.setColor(row, col, Color.black);
                 } else if (grid[row][col] == LASER) {
                     display.setColor(row, col, Color.red);
+                } else if (grid[row][col] == NUKE) {
+                    display.setColor(row, col, Color.red);
+                } else if (grid[row][col] == WOOD) {
+                    // Color anotherColor = new Color(74, 55, 40);
+                    Color anotherColor = new Color(108, 69, 28);
+                    display.setColor(row, col, anotherColor);
                 }
             }
         }
     }
 
-   
     //called repeatedly.
     //causes one random particle to maybe do something.
     public void step()
     {
-        int row = (int)(Math.random() * grid.length);
-        int col = (int)(Math.random() * grid[0].length);
+        int maxRow = grid.length;
+        int maxCol = grid[0].length;
+        int row = (int)(Math.random() * maxRow);
+        int col = (int)(Math.random() * maxCol);
         
-        if (row != grid.length - 1) {
-            if (grid[row][col] == SAND && grid[row + 1][col] != SAND && grid[row + 1][col] != METAL) {
-                grid[row + 1][col] = SAND;
-                grid[row][col] = ERASE;
-            } else if (grid[row][col] == LASER) {
-                grid[row + 1][col] = LASER;
-                grid[row][col] = ERASE;
-                
-            }
+        int g = grid[row][col];
+        switch (g) {
+            case SAND:
+                if (row >= maxRow - 1)
+                    break;
+                // sand replaces water
+                if (grid[row + 1][col] == WATER) {
+                    grid[row + 1][col] = g;
+                    grid[row][col] = WATER;
+                    break;
+                }
+                // sand falls
+                if (grid[row + 1][col] == ERASE) {
+                    grid[row + 1][col] = g;
+                    grid[row][col] = ERASE;
+                    break;
+                }
+                // make pyrimids in water/erase
+                if ((int)(Math.random() * 2) == 0) {
+                    // down and left
+                    if (col >= 1 && (grid[row + 1][col - 1] == WATER
+                        || grid[row + 1][col - 1] == ERASE)) {
+                        grid[row][col] = grid[row + 1][col - 1];
+                        grid[row + 1][col - 1] = g;
+                    }
+                } else { // down and right
+                    if (col < maxCol - 1 && (grid[row + 1][col + 1] == WATER 
+                        || grid[row + 1][col + 1] == ERASE)) {
+                        grid[row][col] = grid[row + 1][col + 1];
+                        grid[row + 1][col + 1] = g;
+                    }
+                }
+                break;
+            case WATER:
+                // Bottom row ignore
+                if (row >= maxRow - 1) 
+                    break;
+                // Falling
+                if (grid[row + 1][col] == ERASE) {
+                    grid[row + 1][col] = g;
+                    grid[row][col] = ERASE;
+                    break;
+                }
+                // Left or right movement
+                if ((int)(Math.random() * 2) == 0) {
+                    // Left movement
+                    if (col >= 1 && grid[row][col - 1] == ERASE) {
+                        grid[row][col - 1] = g;
+                        grid[row][col] = ERASE;
+                    }
+                } else {
+                    // Right movement
+                    if (col < maxCol - 1 && grid[row][col + 1] == ERASE) {
+                        grid[row][col + 1] = g;
+                        grid[row][col] = ERASE;
+                    }
+                }
+                break;
+            case WOOD:
+                if (row > maxRow - 1 && col > maxCol - 1) {
+                    if (grid[row + 1][col] == ERASE) {
+                        grid[row + 1][col] = g;
+                        grid[row][col] = ERASE;
+                        break;
+                    }
+                }
+                break;
+            case LASER:
+                // Erases past laser
+                if (row == maxRow - 1 || col == maxCol - 1) {
+                    grid[row][col] = ERASE;
+                }
+                //Move laser down and to the right
+                if (row + 1 <= maxRow - 1 && col + 1 <= maxCol - 1) {
+                    grid[row][col] = ERASE;
+                    grid[row + 1][col + 1] = LASER;
+                }
+                break;
+            case NUKE:
+                // Erases laser on an edge
+                if (row == maxRow - 1 || col == maxCol - 1) {
+                    grid[row][col] = ERASE;
+                }
+                if (row + 1 <= maxRow - 1 && col + 1 <= maxCol - 1) {
+                    grid[row][col] = ERASE;
+                    grid[row + 1][col + 1] = NUKE;
+                }
+                if (row + 2 <= maxRow - 1 && col - 1 >= 0 && col <= maxCol - 1) {
+                    grid[row + 1][col - 1] = ERASE;
+                    grid[row + 2][col] = NUKE;
+                }
+                if (row <= maxRow - 1 && row - 1 >= 0 && col + 2 <= maxCol - 1) {
+                    grid[row - 1][col + 1] = ERASE;
+                    grid[row][col + 2] = NUKE;
+                }
+                break;
         }
     }
 
-//do not modify
+    //do not modify
     public void run()
     {
         while (true)
@@ -95,4 +209,3 @@ public class SandLab
         }
     }
 }
- 
