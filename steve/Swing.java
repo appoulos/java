68a1cdef523b217e873075554920c702ed65bbb2
Anthony Poulos <anthonypoulos@yahoo.com>
2025-02-09 14:37:28 -0600

    .

--- steve/Swing.java
+++ steve/Swing.java
@@ -1,7 +1,5 @@
 import javax.swing.*;
 
-import java.awt.FlowLayout;
-
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -9,21 +7,10 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 public class Swing {
-	enum enum_ {
-		ace, two, _3, king
-	};
-
-	static String[] ranks = { "ace", "2", "3", "jack" };
 
 	public static void main(String[] args) {
-		int height = 200;
-		// int width = 40;
-		// creating instance of JFrame
 		JFrame f = new JFrame();
-		// ImageIcon a = new ImageIcon("cards/a.png");
-		ImageIcon c = new ImageIcon("cards/2_of_clubs.png");
-		ImageIcon d = null;
-		ImageIcon tile = null;
+
 		BufferedImage tiles = null;
 		try {
 			tiles = ImageIO.read(new File("tiles.png"));
@@ -31,34 +18,64 @@ public class Swing {
 			System.err.println("Error reading image file: " + e.getMessage());
 			System.exit(1);
 		}
+
+		// Create a JPanel to hold the images
+		JPanel imagePanel = new JPanel();
+		imagePanel.setLayout(new GridLayout(13, 4)); // Use FlowLayout or other as needed
+
 		for (int i = 0; i < 52; i++) {
-			tile = new ImageIcon(tiles.getSubimage(207 * i, 0, 207, 300));
-			f.add(new JLabel(tile));
-		}
-		try {
-			BufferedImage originalImage = ImageIO.read(new File("cards/2_of_clubs.png"));
-			Image resizedImage = originalImage.getScaledInstance(-1, height, Image.SCALE_SMOOTH);
-			d = new ImageIcon(resizedImage);
-		} catch (IOException e) {
-			System.err.println("Error reading image file: " + e.getMessage());
-			System.exit(1);
+			imagePanel.add(new JLabel(new ImageIcon(tiles.getSubimage(207 * i, 0, 207, 300))));
 		}
 
-		String str = "";
-		// for (enum_ rank : enum_.values())
-		// str += rank + " ";
-		for (String s : ranks)
-			str += s + " ";
-		JButton b1 = new JButton(str);
-		b1.setBounds(90, 100, 180, 40);
-		f.add(b1);
-		JLabel l1 = new JLabel(c);
-		f.add(l1);
-		JLabel l2 = new JLabel(d);
-		f.add(l2);
+		// Create the JScrollPane
+		JScrollPane scrollPane = new JScrollPane(imagePanel);
+		scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+		scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+
+		// Add the JScrollPane to the frame
+		f.add(scrollPane);
+
+		// private void addImage(JPanel panel, String imagePath) {
+		// try {
+		// ImageIcon imageIcon = new ImageIcon(imagePath);
+		// //Scale image if it's too big
+		// Image image = imageIcon.getImage();
+		// Image scaledImage = image.getScaledInstance(200, 200,
+		// java.awt.Image.SCALE_SMOOTH);
+		// imageIcon = new ImageIcon(scaledImage);
+		// JLabel label = new JLabel(imageIcon);
+		// panel.add(label);
+		// } catch (Exception e) {
+		// System.err.println("Error loading image: " + imagePath);
+		// e.printStackTrace();
+		// }
+		// }
+
+		// try {
+		// BufferedImage originalImage = ImageIO.read(new File("cards/2_of_clubs.png"));
+		// Image resizedImage = originalImage.getScaledInstance(-1, height,
+		// Image.SCALE_SMOOTH);
+		// d = new ImageIcon(resizedImage);
+		// } catch (IOException e) {
+		// System.err.println("Error reading image file: " + e.getMessage());
+		// System.exit(1);
+		// }
+
+		// String str = "";
+		// // for (enum_ rank : enum_.values())
+		// // str += rank + " ";
+		// for (String s : ranks)
+		// str += s + " ";
+		// JButton b1 = new JButton(str);
+		// b1.setBounds(90, 100, 180, 40);
+		// f.add(b1);
+		// JLabel l1 = new JLabel(c);
+		// f.add(l1);
+		// JLabel l2 = new JLabel(d);
+		// f.add(l2);
 
 		f.setSize(400, 400);
-		f.setLayout(new FlowLayout());
+		// f.setLayout(new FlowLayout());
 		f.setVisible(true);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
