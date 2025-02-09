b1774bbff21d5bbe824d41a0dbe25e4ec704e1c3
Anthony Poulos <anthonypoulos@yahoo.com>
2025-02-09 05:30:52 -0600

    .

--- steve/Swing.java
+++ steve/Swing.java
@@ -9,35 +9,55 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 public class Swing {
+	enum enum_ {
+		ace, two, _3, king
+	};
+
+	static String[] ranks = { "ace", "2", "3", "jack" };
+
 	public static void main(String[] args) {
 		int height = 200;
 		// int width = 40;
 		// creating instance of JFrame
 		JFrame f = new JFrame();
-		ImageIcon a = new ImageIcon("cards/a.png");
+		// ImageIcon a = new ImageIcon("cards/a.png");
 		ImageIcon c = new ImageIcon("cards/2_of_clubs.png");
-		ImageIcon d;
+		ImageIcon d = null;
+		ImageIcon tile = null;
 
 		try {
+			BufferedImage tiles = ImageIO.read(new File("cards/tiles.png"));
+			tile = new ImageIcon(tiles.getSubimage(207, 0, 207, 300));
+		} catch (IOException e) {
+			System.err.println("Error reading image file: " + e.getMessage());
+			System.exit(1);
+		}
+		try {
 			BufferedImage originalImage = ImageIO.read(new File("cards/2_of_clubs.png"));
-			// Resize the image
 			Image resizedImage = originalImage.getScaledInstance(-1, height, Image.SCALE_SMOOTH);
 			d = new ImageIcon(resizedImage);
 		} catch (IOException e) {
 			System.err.println("Error reading image file: " + e.getMessage());
-			d = a; // new ImageIcon(); // Empty icon in case of error
+			System.exit(1);
 		}
 
-		JButton b1 = new JButton("Hello, World!", a);
+		String str = "";
+		// for (enum_ rank : enum_.values())
+		// str += rank + " ";
+		for (String s : ranks)
+			str += s + " ";
+		JButton b1 = new JButton(str);
 		b1.setBounds(90, 100, 180, 40);
 		f.add(b1);
 		JLabel l1 = new JLabel(c);
 		f.add(l1);
 		JLabel l2 = new JLabel(d);
 		f.add(l2);
+		f.add(new JLabel(tile));
 
 		f.setSize(400, 400);
 		f.setLayout(new FlowLayout());
 		f.setVisible(true);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
