1a6ec3b369f04317f18ec0cea90095277d33c092
Anthony Poulos <anthonypoulos@yahoo.com>
2025-02-11 10:20:38 -0600

    .

--- steve/BlackJack.java
+++ steve/BlackJack.java
@@ -1,4 +1,5 @@
 import static java.lang.System.out;
+import java.util.Scanner;
 import java.util.ArrayList;
 
 class Card {
@@ -12,7 +13,7 @@ class Card {
 
 	@Override
 	public String toString() {
-		return rank + " of " + suit;
+		return rank; // + " of " + suit;
 	}
 }
 
@@ -35,6 +36,12 @@ class Deck {
 					cards.add(new Card(rank, suit));
 				}
 			}
+		}
+		int deckSize = cards.size();
+		for (int i = 0; i < deckSize; i++) {
+			Card temp = cards.get(i);
+			cards.set((int) (Math.random() * deckSize), cards.get(i));
+			cards.set(i, temp);
 		}
 	}
 
@@ -77,19 +84,40 @@ class Hand {
 	}
 
 	int value() {
-		out.println("todo");
 		int value = 0;
 		int aces = 0;
 		for (Card card : cards) {
 			if (card.rank == "Ace")
 				aces++;
 			switch (card.rank) {
-					case "Ace" -> value += 1;
-				case "2", "3", "4", "5", "6", "7", "8", "9", "10" ->
-					value = Integer.valueOf(card.rank);
-				case "Jack", "Queen", "King" -> value += 10;
-				default ->value +=  0;
+				case "Ace":
+					value += 1;
+					break;
+				case "2":
+				case "3":
+				case "4":
+				case "5":
+				case "6":
+				case "7":
+				case "8":
+				case "9":
+				case "10":
+					value += Integer.valueOf(card.rank);
+					break;
+				case "Jack":
+				case "Queen":
+				case "King":
+					value += 10;
+					break;
+				// default ->value += 0;
 			}
+			// switch (card.rank) {
+			// case "Ace" -> value += 1;
+			// case "2", "3", "4", "5", "6", "7", "8", "9", "10" ->
+			// value = Integer.valueOf(card.rank);
+			// case "Jack", "Queen", "King" -> value += 10;
+			// default ->value += 0;
+			// }
 		}
 		while (aces > 0 && value + 10 <= 21) {
 			value += 10;
@@ -126,9 +154,30 @@ class Dealer {
 	}
 
 	void newRound() {
+		// deal two cards to every player
 		for (Player player : players) {
 			player.giveCard(shoe.getCard());
 			player.giveCard(shoe.getCard());
+		}
+
+		String turn;
+		for (Player player : players) {
+			while (true) {
+				out.println(player);
+				turn = Scan.readLine("Player " + player.name + " (H)it or (s)tand? ");
+				if (turn.equals("s"))
+					break;
+				player.giveCard(shoe.getCard());
+				if (player.hand.value() > 21) {
+					out.println("Player " + player.name + " busts");
+					break;
+				}
+			}
+		}
+
+		out.println("Results: ");
+		for (Player player : players) {
+			out.println("" + player);
 		}
 	}
 }
@@ -156,16 +205,62 @@ class Player {
 	}
 }
 
+class Scan {
+	static Scanner scan = new Scanner(System.in);
+
+	public static String readLine(String prompt) {
+		System.out.print(prompt);
+		return scan.nextLine();
+	}
+
+	public static int readInt(String prompt) {
+		while (true) {
+			String input = readLine(prompt);
+			try {
+				int n = Integer.parseInt(input);
+				return n;
+			} catch (NumberFormatException e) {
+
+			}
+		}
+	}
+}
+
 public class BlackJack {
+
 	public static void main(String[] args) {
-		out.println("hi");
 		ArrayList<Player> players = new ArrayList<>();
-		players.add(new Player("Alice"));
-		players.add(new Player("Suzy"));
+		out.println("Welcome to Black Jack");
+
 		Dealer dealer = new Dealer("Bob", players);
-		dealer.newRound();
-		for (Player player : players) {
-			out.println("" + player);
+
+		int numPlayers = Scan.readInt("How many players? ");
+
+		for (int i = 1; i <= numPlayers; i++) {
+			String name = Scan.readLine("Player " + i + " name? ");
+			players.add(new Player(name));
+
+		}
+		while (true) {
+			dealer.newRound();
+
+			ArrayList<Player> winners = new ArrayList<>();
+			int maxScore = 0;
+			for (Player player : players) {
+				int score = player.hand.value();
+				if (score > maxScore) {
+					maxScore = score;
+					winners.clear();
+					winners.add(player);
+				}
+			}
+			out.println("Winner(s): ");
+			for (Player winner : winners) {
+				out.println(winner);
+			}
+			String again = Scan.readLine("Again (Y/n)? ");
+			if (again.equals("n"))
+				break;
 		}
 	}
 }
