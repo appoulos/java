e8e0301d369816aacced1cac9dc27f8df3daaed9
Anthony Poulos <anthonypoulos@yahoo.com>
2025-05-26 20:59:19 -0500

    .

--- steve/finalproject/Games.java
+++ steve/finalproject/Games.java
@@ -351,11 +351,11 @@ public class Games extends JPanel implements ActionListener, KeyListener, MouseM
 		int posX = gameWidth / 2 - rectWidth;
 		g.drawString("Main Menu", posX, posY);
 		posY += dy;
-		g.drawString(
-				"clicked: " + mouseClickedX + "," + mouseClickedY + " mouse: " + mouseX + ","
-						+ mouseY + " pos: " + posX
-						+ "," + posY + " rect: " + rectWidth + "," + rectHeight,
-				25, 25);
+		// g.drawString(
+		// "clicked: " + mouseClickedX + "," + mouseClickedY + " mouse: " + mouseX + ","
+		// + mouseY + " pos: " + posX
+		// + "," + posY + " rect: " + rectWidth + "," + rectHeight,
+		// 25, 25);
 		for (int i = 0; i < selections.length; i++) {
 			posY += dy;
 			g.drawString(selections[i], posX, posY);
